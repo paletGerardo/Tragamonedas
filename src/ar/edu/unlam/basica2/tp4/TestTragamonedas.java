73ca38f9a1ff298d733ce4681ef6868ef2007563
Hugo Gerardo Palet <paletgerardo@gmail.com>
2016-06-24 07:06:56 -0300

    TP completo, pero sigo sin poder poner la clase test dentro del package correspondiente. Cuando lo hago se arma una cascada de carpetas y no funciona cuando quiero correr el test.

--- src/ar/edu/unlam/basica2/tp4/TestTragamonedas.java
+++ src/ar/edu/unlam/basica2/tp4/TestTragamonedas.java
@@ -1,5 +1,8 @@
 package ar.edu.unlam.basica2.tp4;
 
+import static org.junit.Assert.*;
+//import javax.swing.JOptionPane;
+
 import javax.swing.JOptionPane;
 
 import org.junit.Test;
@@ -7,35 +10,55 @@ import org.junit.Test;
 public class TestTragamonedas {
 
 	@Test
-	public void testCrearUnTragamonedas() {
+	public void testCrearUnTragamonedasEnPosicionUno() {
 		
 		Tragamonedas maquina1 = new Tragamonedas();
-		System.out.println(maquina1.tambor1.getPosicion());
-		System.out.println(maquina1.tambor2.getPosicion());	
-		System.out.println(maquina1.tambor3.getPosicion());
-		System.out.println("----------------------------------------------");
-		System.out.println("El traga monedas se inicio correctamente.");
-		System.out.println("Dale play y comienza a ganar :)");
-		System.out.println("");
-
-		do {
-			maquina1.activar();
-			System.out.println(maquina1.tambor1.getPosicion());
-			System.out.println(maquina1.tambor2.getPosicion());	
-			System.out.println(maquina1.tambor3.getPosicion());
-			System.out.println("----------------------------------------------");
-			
-			if (maquina1.entregaPremio()) {
-				System.out.println("You Win!...");
-				JOptionPane.showMessageDialog(null, "En hora buena. Ganaste!...");
-			}
-			else{
-				System.out.println("You lose!... Try again.");
-				JOptionPane.showMessageDialog(null, "Intenta nuevamente");
-			}
-			
-			
-		} while (!maquina1.entregaPremio());
+		assertTrue(maquina1.tambor1.getPosicion()==1 && maquina1.tambor2.getPosicion()==1 && maquina1.tambor3.getPosicion()==1);
+		
 	}
+	
+	@Test
+	public void testPruebaRandomNumberUnoADiez() {
+		
+		Integer numeroGenerado;
+		RandomNumberGenerator random1a10 = new RandomNumberGenerator();
+		numeroGenerado = random1a10.generate();
+		assertTrue(numeroGenerado >= 1 && numeroGenerado <=10);
+		
+	}
+	
+//	@Test
+//	public void testPruebaEnConsola(){
+//		
+//		Tragamonedas maquina1 = new Tragamonedas();
+//		
+//		System.out.println(maquina1.tambor1.getPosicion());
+//		System.out.println(maquina1.tambor2.getPosicion());	
+//		System.out.println(maquina1.tambor3.getPosicion());
+//		System.out.println("----------------------------------------------");
+//		System.out.println("El traga monedas se inicio correctamente.");
+//		System.out.println("Dale play y comienza a ganar :)");
+//		System.out.println("");
+//
+//		do {
+//			maquina1.activar();
+//			System.out.println(maquina1.tambor1.getPosicion());
+//			System.out.println(maquina1.tambor2.getPosicion());	
+//			System.out.println(maquina1.tambor3.getPosicion());
+//			System.out.println("----------------------------------------------");
+//			
+//			if (maquina1.entregaPremio()) {
+//				System.out.println("You Win!...");
+//				JOptionPane.showMessageDialog(null, "En hora buena. Ganaste!...");
+//			}
+//			else{
+//				System.out.println("You lose!... Try again.");
+//				JOptionPane.showMessageDialog(null, "Intenta nuevamente");
+//			}
+//			
+//			
+//		} while (!maquina1.entregaPremio());
+//	}		
+		
 
 }
--- src/ar/edu/unlam/basica2/tp4/Tragamonedas.java
+++ src/ar/edu/unlam/basica2/tp4/Tragamonedas.java
@@ -2,6 +2,7 @@ package ar.edu.unlam.basica2.tp4;
 
 	public class Tragamonedas {
 		
+				
 				Tambor tambor1;
 				Tambor tambor2;
 				Tambor tambor3;
@@ -37,31 +38,4 @@ package ar.edu.unlam.basica2.tp4;
 					else return false;
 				}
 		
-		public class Tambor {
-				
-				Integer posicion;
-				
-				public Tambor(NumberGenerator generator) {
-					this.posicion = generator.generate();
-				} 
-				 
-				/* post: devuelve el número de posición en la que se 
-				* encuentra el Tambor. Es un valor comprendido 
-				* entre 1 y 10. 
-				 */
-				
-				public Integer getPosicion() {
-					
-					return this.posicion;
-				} 
-				 
-				/* post: hace girar el tambor y luego se detiene en 
-				* una posición comprendida entre 1 y 10. 
-				*/ 
-				public	void girar() {
-					
-					RandomNumberGenerator numeroAleatorio = new RandomNumberGenerator();
-					this.posicion = numeroAleatorio.generate();
-				}
-			}
 }
